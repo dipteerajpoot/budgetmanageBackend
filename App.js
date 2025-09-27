6adce331bebd76dbf73cc4fa4e8c987789703a8b
dipteerajpoot <rajputdiptee1538@gmail.com>
2025-09-27 13:06:22 +0530

    new changes

--- App.js
+++ App.js
@@ -14,8 +14,10 @@ const app = express();
 
 // -------------------- Middleware --------------------
 app.use(cors({
-    origin: "http://localhost:3001",
-    credentials: true
+    origin: ["http://localhost:3000", "http://localhost:3001"],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"]
 }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
